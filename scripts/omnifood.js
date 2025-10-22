f3d8dd54511b01718b9674e3a3e7dcb71a5a7191
Chinenye Dominic Eloji <elojichinenye24@gmail.com>
2025-10-23 00:47:42 +0100

    Compeleted and analyzed the foodie page

--- scripts/omnifood.js
+++ scripts/omnifood.js
@@ -11,7 +11,7 @@ const actionBg = document.querySelector(".action-bg");
 window.addEventListener("scroll", function(){
   if (window.scrollY > 100){
     header.classList.add("active");
-    actionBg.classList.add("active")
+    actionBg.classList.add("active");
   }else{
     header.classList.remove("active");
     actionBg.classList.remove("active");
